865ffbd55f316d376343c8882f322601dca1c6b6
kangmantul <jordytandiano@gmail.com>
2025-06-02 11:37:08 +0700

    LIGHT IMPLEMENT AND FIX SPAWNER BUG

--- Assets/Scripts/SpawnerManager.cs
+++ Assets/Scripts/SpawnerManager.cs
@@ -5,28 +5,70 @@ using UnityEngine;
 public class SpawnerManager : MonoBehaviour
 {
     public GameObject platformPrefab;
+    public GameObject[] obstaclePrefabs;
+    public GameObject lightPrefab;
+
     public Transform player;
-    public float spawnDistance = 2f;
-    private float lastSpawnY;
+
+    public float minX = -2.5f;
+    public float maxX = 2.5f;
+    public float minYGap = 1.5f;
+    public float maxYGap = 3f;
+    public float obstacleChance = 0.3f;
+    public float lightChance = 0.7f;
+
+    private float nextSpawnY;
+    private float lastX;
 
     void Start()
     {
-        lastSpawnY = player.position.y;
+        nextSpawnY = player.position.y;
+
+        for (int i = 0; i < 10; i++)
+        {
+            SpawnNext();
+        }
     }
 
     void Update()
     {
-        if (player.position.y > lastSpawnY - spawnDistance)
+        if (player.position.y + 10f > nextSpawnY)
         {
-            SpawnNextObs();
-            lastSpawnY += spawnDistance;
+            SpawnNext();
         }
     }
 
-    void SpawnNextObs()
+    void SpawnNext()
     {
-        float randomX = Random.Range(-2f, 2f);
-        Vector2 spawnPos = new Vector2(randomX, lastSpawnY + spawnDistance);
-        Instantiate(platformPrefab, spawnPos, Quaternion.identity);
+        float randomX;
+
+        do
+        {
+            randomX = Random.Range(minX, maxX);
+        } while (Mathf.Abs(randomX - lastX) < 0.5f);
+
+        lastX = randomX;
+
+        float yGap = Mathf.Lerp(minYGap, maxYGap, Random.value * Random.value);
+        nextSpawnY += yGap;
+
+        Vector2 spawnPos = new Vector2(randomX, nextSpawnY);
+
+        GameObject platform = Instantiate(platformPrefab, spawnPos, Quaternion.identity);
+        platform.tag = "Platform";
+
+        if (obstaclePrefabs.Length > 0 && Random.value < obstacleChance)
+        {
+            int index = Random.Range(0, obstaclePrefabs.Length);
+            Vector3 obstaclePos = spawnPos + Vector2.up * 0.5f;
+            GameObject obs = Instantiate(obstaclePrefabs[index], obstaclePos, Quaternion.identity);
+            obs.tag = "Obstacle";
+        }
+
+        if (lightPrefab != null && Random.value < lightChance)
+        {
+            Vector3 lightPos = spawnPos + Vector2.up * 2f;
+            Instantiate(lightPrefab, lightPos, Quaternion.identity);
+        }
     }
 }
